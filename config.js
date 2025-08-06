cc96f6102454fad6a379f5529bda29f8c90a37fa
Suleman Ahmad <ahmadsuleman726@gmail.com>
2025-08-05 19:51:45 -0600

    Adding files

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 import OpenAI from 'https://cdn.jsdelivr.net/npm/openai/+esm';
-const api_key = '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';
-const SUPABASE_API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InprbXdtYWhqY2VyZXZ0bmhqeHdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNDgxOTksImV4cCI6MjA2NjgyNDE5OX0.u0ymMl-bHTlGtMMnqcq0MlnnW6_xt1bhd1UGaZYBoHU'
-const SUPABASE_URL = 'https://zkmwmahjcerevtnhjxwo.supabase.co'
+const api_key = ''
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-export const TMDB_KEY = '1e534f0e2e56d8be449a58f8d246dacb'
+export const TMDB_KEY = ''
 
 /** OpenAI config */
 if (!api_key) throw new Error("OpenAI API key is missing or invalid.");
